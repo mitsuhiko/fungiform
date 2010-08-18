8e021652aa4580192b9a7492dbfc0840125faa7f
Armin Ronacher <armin.ronacher@active-4.com>
2010-08-18 11:35:57 +0200

    Added text to the __init__.py file

---
+++ fungiform/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    fungiform
+    ~~~~~~~~~
+
+    Form library and CSRF protection system.
+
+    :copyright: (c) 2010 by the Fungiform Team.
+    :license: BSD, see LICENSE for more details.
+"""
